35f7acbd4c4eb763e2e35652f12fcc6096e50692
Documenter.jl <documenter@juliadocs.github.io>
2024-11-30 22:36:08 +0000

    build based on f71ff3a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KenwardRoger","category":"page"},{"location":"#KenwardRoger","page":"Home","title":"KenwardRoger","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KenwardRoger.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KenwardRoger]","category":"page"}]
+}
